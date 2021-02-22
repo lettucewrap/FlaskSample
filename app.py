2844ad47de4812a0cf25f2768a257641331aa365
Sarai Alvarez <sarai_alvarez86@hotmail.com>
2021-02-22 00:46:29 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/welcome/<name>')
+def dashboard(name):
+    return 'Welcome %s!' % name
+
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        return redirect(url_for('dashboard', name=name))
+    else:
+        name = request.args.get('name')
+        return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
